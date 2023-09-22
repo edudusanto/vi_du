07ea7ea752c5c12574e8c2d12b6fb014c1f3b291
edudusanto <142506588+edudusanto@users.noreply.github.com>
2023-09-22 10:22:36 -0300

    Add files via upload

--- vi_edu/index.js
+++ vi_edu/index.js
@@ -1,23 +1,26 @@
 //nota do primeiro trimestre//
-let nota1 = ducument.querySelector(".trim1").textContent;
+let nota1 = parseFloat(ducument.querySelector(".trim1").textContent);
+console.log(nota1Trim);
+//nota do segundo trimestre//
+let nota2 = parseFloat(ducument.querySelector(".trim2").textContent);
+console.log(nota2Trim);
+//nota do terceiro trimestre//
+let nota3 = parseFloat(ducument.querySelector(".trim3").textContent);
+console.log(nota3Trim);
 
-//seegundo tri//
-let nota2 = ducument.querySelector(".trim2").textContent;
-//terceiro trimestre//
-let nota3 = ducumentSelector(".trim3").textContent;
-
-let notaFinal = ducument.querySelector(".nota").textContent;
-//calculo//
-
-
-
-let resultado = nota1+nota2+nota3;
+//cálculo//
+let resultado = nota1 + nota2 + nota3;
 console.log(resultado);
+//cria a variável e armazena o conteudo do resultado final//
+let resultadoFinal = document.querySelector(".resultado-final");
 
-if(nota1 === true && nota2 ===  nota2 ===  nota3 === true){
-    let resultado = nota1+nota2+nota3;
-    console.log(resultado);
-    notaFinal.textContent = resultado.toFixed; 
+if(resultado >= 180){
+    //se for maior que 180 aparece como aprovado
+    resultadoFinal.textContent = "Sim, parabéns!!!";
+} else{
+    //se não, aparece como reprovado e mostra o quanto de nota precisa
+    let notaNecessaria = 180-(mota1Trim+nota2Trim);
+    resultadoFinal.textContent = "Não! Melhore!";
 }
 
  
