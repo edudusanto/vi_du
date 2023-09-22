760253d70afc735fa8e197b06ff55eb20a6cbcd6
edudusanto <142506588+edudusanto@users.noreply.github.com>
2023-09-22 08:25:54 -0300

    Add files via upload

---
+++ vi_edu/index.js
@@ -0,0 +1,23 @@
+//nota do primeiro trimestre//
+let nota1 = ducument.querySelector(".trim1").textContent;
+
+//seegundo tri//
+let nota2 = ducument.querySelector(".trim2").textContent;
+//terceiro trimestre//
+let nota3 = ducumentSelector(".trim3").textContent;
+
+let notaFinal = ducument.querySelector(".nota").textContent;
+//calculo//
+
+
+
+let resultado = nota1+nota2+nota3;
+console.log(resultado);
+
+if(nota1 === true && nota2 ===  nota2 ===  nota3 === true){
+    let resultado = nota1+nota2+nota3;
+    console.log(resultado);
+    notaFinal.textContent = resultado.toFixed; 
+}
+
+ 
